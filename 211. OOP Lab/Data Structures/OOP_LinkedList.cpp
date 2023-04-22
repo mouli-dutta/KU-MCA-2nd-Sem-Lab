ca8143c7e7c22714cc3356344b4ac6154590f464
MouliDutta <moulidutta44@gamil.com>
2023-04-22 15:12:52 +0530

    Added more functions

--- 211. OOP Lab/Data Structures/OOP_LinkedList.cpp
+++ 211. OOP Lab/Data Structures/OOP_LinkedList.cpp
@@ -2,7 +2,9 @@
 
 using namespace std;
 
+//---------------------------------------------------------
 // Node class to represent a single node in the linked list
+//---------------------------------------------------------
 template<typename T>
 class Node {
 private:
@@ -19,7 +21,10 @@ public:
     void setNext(Node* next);
 };
 
+
+// ---------------------------------------------------------
 // Linked list class to manage the nodes
+// ---------------------------------------------------------
 template<typename T>
 class LinkedList {
 private:
@@ -46,7 +51,11 @@ public:
 
     void append(LinkedList<T> &another);
 
-    void insertion_sort();
+    void insertion_sort(bool (*compare)(T, T));
+
+    bool isFound(T target, bool (*equals)(T, T));
+
+    void reverse();
 
     void print();
 
@@ -54,50 +63,134 @@ public:
 };
 
 
+
+// --------------------------------------------------------------
+
+// for taking user input
+int getInput(string promt) {
+    cout << promt;
+    int inp;
+    cin >> inp;
+    return inp;
+}
+
+// function used in insertion_sort method for sorting in ascending order
+bool compare(int a, int b) {
+    return a < b;
+}
+
+// function used in search method to check if two values are equal
+bool equals(int a, int b) {
+    return a == b;
+}
+
+
+
 // Main function to test the linked list implementation
 int main() {
-    LinkedList<int> intList;
+    cout << "\tMenu:\n"
+         << "1. Insert node at beginning.\n"
+         << "2. Insert node at index (index starts from 0).\n"
+         << "3. Insert node at end.\n"
+         << "4. Delete first node.\n"
+         << "5. Delete node at index (index starts from 0)."
+         << "6. Print length of the list.\n"
+         << "7. Sort the list.\n"
+         << "8. Search for data in the list.\n"
+         << "9. Reverse the list.\n"
+         << "10. Concate two lists.\n"
+         << "11. Print the list.\n"
+         << "12. Exit.\n\n";
 
-    intList.insert_begin(5);
-    intList.insert_begin(10);
+    LinkedList<int> list;
 
-    intList.print();
-    cout << "Length = " << intList.getLen() << "\n";
+    while(true) {
+        int choice = getInput("Enter your choice.\n");
 
+        switch (choice)
+        {
+        case 1:
+            list.insert_begin(
+                getInput("Enter the data you want to insert.\n")
+            );
+            cout << "Inserted.\n";
+            break;
+        
+        case 2:
+            list.insert_at(
+                getInput("Enter the data.\n"),
+                getInput("Enter the index.\n")
+            );
+            cout << "Inserted.\n";
+            break;
+        
+        case 3:
+            list.insert_end(
+                getInput("Enter the data.\n")
+            );
+            cout << "Inserted.\n";
+            break;
+        
+        case 4:
+            list.delete_front();
+            cout << "Deleted.\n";
+            break;
+        
+        case 5:
+            list.delete_at(
+                getInput("Enter the index.\n")
+            );
+            cout << "Deleted.\n";
+            break;
+        
+        case 6:
+            list.delete_end();
+            cout << "Deleted.\n";
+            break;
+        
+        case 7:
+            list.insertion_sort(compare);
+            cout << "Sorted.\n";
+            break;
+        
+        case 8:
+            {
+                int target = getInput("Enter the value you want to search.\n");
+                if(list.isFound(target, equals)) 
+                    cout << "Target found.\n";
+                else
+                    cout << "Target not found.\n";
+            }
 
-    LinkedList<int> intList2;
+            break;
+        
+        case 9:
+            list.reverse();
+            cout << "Reversed.\n";
+            break;
+        
+        // TODO: append another list
+        case 10:
+            
+            break;
+        
+        case 11:
+            cout << "Your list:\n";
+            list.print();
+            break;
 
-    intList2.insert_begin(25);
-    intList2.insert_end(15);
-    intList2.insert_at(2, 1);
-
-    intList2.print();
-    cout << "Length = " << intList2.getLen() << "\n";
-
-    intList.append(intList2);
-    intList.print();
-    cout << "Length = " << intList.getLen() << "\n";
-
-    intList.insertion_sort();
-    intList.print();
-    cout << "Length = " << intList.getLen() << "\n";
-
-    LinkedList<string> stringList;
-
-    stringList.insert_begin("hello");
-    stringList.insert_begin("world");
-    stringList.insert_at("beautiful", 1);
-
-    stringList.print();
-    cout << "Length = " << stringList.getLen() << "\n";
-
-    stringList.delete_front();
-
-    stringList.print();
-    cout << "Length = " << stringList.getLen() << "\n";
+        default:
+            cout << "Terminating the program.\n";
+            exit(0);
+            break;
+        }
+    }
 
     return 0;
 }
+
+
+
 
 
 
@@ -135,6 +228,7 @@ template <typename T>
 void Node<T>::setNext(Node *next) {
     this->next = next;
 }
+
 
 
 
@@ -271,7 +365,7 @@ void LinkedList<T>::delete_at(int index)
         prev = curr;
         curr = curr->getNext();
     }
-    prev->getNext()->setNext(curr->getNext());
+    prev->setNext(curr->getNext());
     delete curr;
     len--;
 }
@@ -328,7 +422,7 @@ void LinkedList<T>::append(LinkedList<T> &another)
 
 // Method to sort the list using Insertion sort algorithm
 template <typename T>
-void LinkedList<T>::insertion_sort()
+void LinkedList<T>::insertion_sort(bool (*compare)(T, T))
 {
     if(!head || !head->getNext()) {
         cout << "List is already sorted.\n";
@@ -341,14 +435,14 @@ void LinkedList<T>::insertion_sort()
     while(current) {
         Node<T> *nextNode = current->getNext();
 
-        if(!sortedList || current->getData() < sortedList->getData()) {
+        if(!sortedList || compare(current->getData(), sortedList->getData())) {
             current->setNext(sortedList);
             sortedList = current;
 
         } else {
             Node<T> *temp = sortedList;
             while(temp) {
-                if(!temp->getNext() || current->getData() < temp->getNext()->getData()) {
+                if(!temp->getNext() || compare(current->getData(), temp->getNext()->getData())) {
                     current->setNext(temp->getNext());
                     temp->setNext(current);
                     break;
@@ -361,6 +455,41 @@ void LinkedList<T>::insertion_sort()
     head = sortedList;
 }
 
+template <typename T>
+bool LinkedList<T>::isFound(T target, bool (*equals)(T, T))
+{
+    bool found = false;
+    Node<T> *curr = head;
+    while(curr) {
+        if(equals(curr->getData(), target)) {
+            found = true;
+            break;
+        }
+        curr = curr->getNext();
+    }
+    return found;
+}
+
+template <typename T>
+void LinkedList<T>::reverse()
+{
+    if(!head) {
+        cout << "List is empty.\n";
+        return;
+    }
+
+    Node<T> *curr = head;
+    Node<T> *prev = nullptr, *next = nullptr;
+
+    while(curr) {
+        next = curr->getNext();
+        curr->setNext(prev);
+
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+}
 
 // Method to print the contents of the list
 template <typename T>
