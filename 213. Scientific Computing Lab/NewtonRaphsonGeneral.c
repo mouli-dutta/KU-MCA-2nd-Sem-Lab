92ce58cc3123a37021b1f52c9043d481bb4f9c15
Mouli Dutta <117016206+mouli-dutta@users.noreply.github.com>
2023-04-27 06:20:21 +0530

    Updated with LinkedList

--- 213. Scientific Computing Lab/NewtonRaphsonGeneral.c
+++ 213. Scientific Computing Lab/NewtonRaphsonGeneral.c
@@ -8,27 +8,40 @@ typedef struct  Polynomial
 {
     int degree;
     float coefficient;
+    struct Polynomial *next;
 } Poly;
 
+Poly *newNode(int degree, float coeff) {
+    Poly *new_poly = malloc(sizeof(Poly));
+    new_poly->degree = degree;
+    new_poly->coefficient = coeff;
+    new_poly->next = NULL;
+    return new_poly;
+}
+
+void insert(Poly **head, int degree, float coeff) {
+    Poly *new_poly = newNode(degree, coeff);
+    if (!head || !(*head))
+    {
+        *head = new_poly;
+    }
+    else {
+        new_poly->next = *head;
+        *head = new_poly;
+    }
+}
 
 // build polynomial equation
 Poly* takeInput(int degree) {
     int size = degree + 1;
 
-    Poly *eqn = malloc(sizeof(Poly)*size);
+    Poly *eqn = NULL;
 
-    while(degree != -1) {
-        printf("\nEnter the coefficient of x^%d\n", degree);
+    for(int i = 0; i < degree+1; i++) {
+        printf("\nEnter the coefficient of x^%d\n", i);
         float coeff;
         scanf("%f", &coeff);
-
-        Poly node;
-        node.degree = degree;
-        node.coefficient = coeff;
-
-        eqn[size-degree-1] = node;
-
-        --degree;
+        insert(&eqn, i, coeff);
     }
 
     return eqn;
@@ -36,46 +49,61 @@ Poly* takeInput(int degree) {
 }
 
 // differentiate given polynomial
-Poly* derivative(Poly *eqn, int size) {
-    Poly *diffArr = malloc(sizeof(Poly)*size);
+Poly* derivative(Poly *eqn) {
+    Poly *diff = NULL;
 
-    for(int i = 0; i < size; ++i) {
-        Poly diff;
-        diff.degree = eqn[i].degree - 1;
-        diff.coefficient = eqn[i].coefficient * eqn[i].degree;
+    while(eqn) {
+        if (eqn->degree > 0)
+            insert(&diff, eqn->degree - 1, eqn->coefficient * eqn->degree);
 
-        diffArr[i] = diff;
+        eqn = eqn->next;
     }
-    return diffArr;
+
+    return diff;
 }
 
-float fx(float x, Poly *eqn, int size) {
-    float sum;
-    for(int i = 0; i < size; i++) {
-        sum += eqn[i].coefficient * pow(x, eqn[i].degree);
-        
+// sum the polynomial with given x value
+float fx(float x, Poly *eqn) {
+    float sum = 0.0;
+
+    while(eqn) {
+        sum += eqn->coefficient * pow(x, eqn->degree);
+        eqn = eqn->next;
     }
+
     return sum;
 }
 
-float newton(float x, int size, Poly *eqn, Poly* diff) {
-    float h = fx(x, eqn, size) / fx(x, diff, size-1);
+float newton_raphson(float x, Poly *eqn, Poly* diff) {
+    float h = fx(x, eqn) / fx(x, diff);
+
+    printf("\n\tx\t|\tf(x)\t|\tf`(x)\t|\th");
+
     while(fabs(h) >= E) {
-        h = h = fx(x, eqn, size) / fx(x, diff, size-1);
+
+        printf("\n\t%.5f\t|\t%.5f\t|\t%.5f\t|\t%.5f\t", x, fx(x, eqn), fx(x, diff), h);
+
+        h = fx(x, eqn) / fx(x, diff);
         x = x - h;
     }
+
+    printf("\n\n");
+
     return x;
 
 }
 
-void printPoly(Poly *eqn, int size) {
-    printf("\n\nYour equation: ");
+void printPoly(Poly *eqn) {
+    printf("\nYour equation: ");
 
-    for(int i = 0; i < size; i++) {
-        if(eqn[i].coefficient > 0)
-            printf(" + %.1fx^%d ", eqn[i].coefficient, eqn[i].degree);
-        else
-            printf(" %.1fx^%d ", eqn[i].coefficient, eqn[i].degree);
+    while(eqn) {
+        if (eqn->coefficient > 0)
+        {
+            printf(" + %.1fx^%d ", eqn->coefficient, eqn->degree);
+        } else {
+            printf(" %.1fx^%d ", eqn->coefficient, eqn->degree);
+        }
+        eqn = eqn->next;
     }
 
     printf("\n\n");
@@ -92,18 +120,26 @@ double df(double x) {
     return 2*x + 5;
 }
 
-double newton_raphson(double x) {
+double newton(double x) {
     double h = f(x) / df(x);
+
+    printf("\n\tx\t|\tf(x)\t|\tf`(x)\t|\th");
+
     while(fabs(h) >= E) {
+        printf("\n\t%.5lf\t|\t%.5lf\t|\t%.5lf\t|\t%.5lf\t", x, f(x), df(x), h);
+
         h = f(x) / df(x);
         x = x - h;
     }
+
+    printf("\n\n");
+
     return x;
 }
 
 int main() {
     double x = 1;
-    double root = newton_raphson(x);
+    double root = newton(x);
     printf("The value of approximate root upto 5 decimal places is: %0.5lf", root);
     printf("\nValue of the function at root %lf", f(root));
 
@@ -111,15 +147,14 @@ int main() {
     printf("\n\nEnter the degree of the equation.\n");
     int degree;
     scanf("%d", &degree);
-    int size = degree + 1;
     Poly *eqn = takeInput(degree);
-    printPoly(eqn, size);
+    printPoly(eqn);
 
-    Poly *diff = derivative(eqn, size);
-    printPoly(diff, size-1);
+    Poly *diff = derivative(eqn);
+    printPoly(diff);
 
     float x1 = 1;
-    float root1 = newton(x1, size, eqn, diff);
+    float root1 = newton_raphson(x1, eqn, diff);
     printf("The value of approximate root upto 5 decimal places is: %0.5f", root1);
 
     free(eqn);
