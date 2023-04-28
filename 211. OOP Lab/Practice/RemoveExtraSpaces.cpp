4754ba198bb722a7366aa28161d1bfd7684d7f9f
Mouli Dutta <117016206+mouli-dutta@users.noreply.github.com>
2023-04-28 17:28:54 +0530

    initial commit

--- 211. OOP Lab/Practice/RemoveExtraSpaces.cpp
+++ 211. OOP Lab/Practice/RemoveExtraSpaces.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
 
+using namespace std;
+
+class MyString {
+private:
+    string str;
+
+public:
+    MyString(string str) {
+        this->str = str;
+    }
+
+    string modify();
+};
+
+string MyString::modify()
+{   
+    int len = str.length();
+    string res = "";
+
+    for(int i = 0; i < len; i++) {
+        if(!(s[i]== ' ' && s[i+1]== ' ')) {
+            res += s[i];
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    string str;
+    getline(cin, str);
+    MyString s(str);
+    cout << s.modify() << "\n";
+    return 0;
+}
